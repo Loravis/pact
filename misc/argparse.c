40f00ddbdd0cd12f2a3dff43a1e96c9de7df8aea
Loravis <loravis@proton.me>
2025-03-07 11:19:52 +0100

    Fixed a mistake in argparse.c

--- misc/argparse.c
+++ misc/argparse.c
@@ -10,9 +10,12 @@ int arg_parse(char** buf, int argc, char **argv) {
         if (length > MAXARGUMENTLENGTH) {
             length = MAXARGUMENTLENGTH;
         }
-        char* str = (char*)malloc(length + 1);
+        char* str = (char*)malloc(length + 1); // Adding +1 to make space for a null terminator
+        if (str == NULL) {
+            return -1;
+        }
         strncpy(str, argv[i], length);
-        str[length + 1] = '\0';
+        str[length] = '\0'; // Ensures the string is null terminated
         buf[i - 1] = str;
     }
     return 0;
